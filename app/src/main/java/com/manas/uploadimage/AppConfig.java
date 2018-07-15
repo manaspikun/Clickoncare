e23dccad0a6f49cbffe05096415bc05fd4693c83
manasranjan <manasmahanta92@gmail.com>
2018-07-15 12:26:31 +0530

    Initial commit

---
+++ app/src/main/java/com/manas/uploadimage/AppConfig.java
@@ -0,0 +1,12 @@
+package com.manas.uploadimage;
+
+/**
+ * Created by Moin Khan on 2/21/2017.
+ */
+public class AppConfig {
+
+    // local
+    public static String BASE_URL = "http://192.168.20.137/webservicev2/";
+
+    public static String uploadImage = BASE_URL + "uploadImage.php";
+}
